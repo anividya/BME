2302f39198c8094394cf76c1c3867875e34b138b
aneesh <aneeshbmedip@gmail.com>
2023-11-10 11:11:50 +0400

    first commit

---
+++ BMEAPP/urls.py
@@ -0,0 +1,17 @@
+from django.urls import path,include
+from . import views
+from django.conf import settings
+from django.conf.urls.static import static
+#from django.conf.urls.static import static
+from django.views.static import serve
+
+from django.conf import settings
+from django.views.static import serve
+
+
+urlpatterns = [
+
+    path('',views.index,name='index'),
+    
+    ]
+
---
+++ static/table.js
@@ -0,0 +1,3 @@
+$(document).ready(function () {
+    $('#assetlist').DataTable({ stateSave: true });
+});
